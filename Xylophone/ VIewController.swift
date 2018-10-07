b4996c6c507454f28eaa1526c8b26b9b55ffacf9
Pierre-Luc Bruyere <proteama@yahoo.com>
2018-10-06 20:23:27 -0400

    Added sound effects

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,21 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController
 {
-
+  private lazy var playerNotes = [AVAudioPlayer?]()
 
   override func viewDidLoad()
   {
     super.viewDidLoad()
+
+    for index in 1...7
+    {
+      let url = Bundle.main.url(forResource: "note" + String(index), withExtension: "wav")!
+      do
+      {
+        let player = try AVAudioPlayer(contentsOf: url)
+        playerNotes.append(player)
+      }
+      catch let error
+      {
+        print(error.localizedDescription + " error loading \(url)")
+        playerNotes.append(nil)
+      }
+    }
   }
-
-
 
   @IBAction func notePressed(_ sender: UIButton)
   {
+    guard let player = playerNotes[sender.tag - 1]
+    else
+    {
+      return
+    }
 
+    player.prepareToPlay()
+    player.play()
   }
 }
 
